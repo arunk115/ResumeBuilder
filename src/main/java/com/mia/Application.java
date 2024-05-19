81c9e0c178b5b3cf40656521d8fda261508f8dd1
arunk115 <arunkavi115@gmail.com>
2024-05-19 12:28:39 +0530

    second commit

--- src/main/java/com/mia/Application.java
+++ src/main/java/com/mia/Application.java
@@ -9,34 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
 	public static void main(final String[] args) {
-	//	java.security.Security.setProperty("networkaddress.cache.ttl" , "0");
 		SpringApplication.run(Application.class, args); 
 	}
 
-//	@Override
-//	protected SpringApplicationBuilder configure(final SpringApplicationBuilder application) {
-//		final Map<String, Object> props = new HashMap<>();
-//        props.put("spring.config.name", "nlp");
-//        application.properties(props);
-//		return application.sources(Application.class);
-//	}
-//
-//	private ApplicationContext getExistingRootWebApplicationContext(
-//			final ServletContext servletContext) {
-//		final Object context = servletContext.getAttribute(
-//				WebApplicationContext.ROOT_WEB_APPLICATION_CONTEXT_ATTRIBUTE);
-//		if (context instanceof ApplicationContext) {
-//			return (ApplicationContext) context;
-//		}
-//		return null;
-//	}
-//
-//	@Override
-//	public void onStartup(final ServletContext servletContext) throws ServletException {
-//		super.onStartup(servletContext);
-//		this.applicationContext = getExistingRootWebApplicationContext(servletContext);
-//		if(env==null){
-//			env = applicationContext.getBean(Environment.class);
-//		}
-//	}
 }
--- src/main/java/com/mia/controller/HomeController.java
+++ src/main/java/com/mia/controller/HomeController.java
@@ -1,13 +1,26 @@
 package com.mia.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.mia.reporting.HtmlToPdfConverter;
+import com.mia.reporting.VelocityTemplateParser;
+
 @Controller
 public class HomeController {
+	
+	@Autowired
+	private VelocityTemplateParser velocityTemplateParser;
+	
+	@Autowired
+	private HtmlToPdfConverter htmlToPdfConverter;
 
 	@GetMapping("/")
 	public String homePage() {
+		String html = velocityTemplateParser.generateHTML(null, null);
+		System.out.println(html);
+		htmlToPdfConverter.generatePdf(html);
 		return "index.html";
 	}
 	
